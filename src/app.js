e0247f33f7652062a515abb1888431b64e3459ee
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-16 16:52:34 +0300

    feat: project addition rendering is now filtering

--- src/app.js
+++ src/app.js
@@ -95,7 +95,15 @@ var ProjectList = /** @class */ (function () {
         this.element = importedNode.firstElementChild;
         this.element.id = "".concat(this.type, "-projects");
         projectState.addListener(function (projects) {
-            _this.assignedProjects = projects;
+            var relevantProjects = projects.filter(function (prj) {
+                if (_this.type === "active") {
+                    return prj.status === ProjectStatus.Active;
+                }
+                else {
+                    return prj.status === ProjectStatus.Finished;
+                }
+            });
+            _this.assignedProjects = relevantProjects;
             _this.renderProjects();
         });
         this.attach();
@@ -103,6 +111,7 @@ var ProjectList = /** @class */ (function () {
     }
     ProjectList.prototype.renderProjects = function () {
         var listEl = document.getElementById("".concat(this.type, "-projects-list"));
+        listEl.innerHTML = "";
         for (var _i = 0, _a = this.assignedProjects; _i < _a.length; _i++) {
             var prjItem = _a[_i];
             var listItem = document.createElement("li");
