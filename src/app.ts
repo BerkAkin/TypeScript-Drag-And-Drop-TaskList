39a9bb3670bab7be73c6b1511350febb9d1f442b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-05-16 18:52:27 +0300

    fix: people count is now more informational

--- src/app.ts
+++ src/app.ts
@@ -220,6 +220,15 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
 
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 Person";
+    } else {
+      return `${this.project.people} People`;
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super("single-project", hostId, false, project.id);
     this.project = project;
@@ -229,7 +238,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   configure() {}
   renderContent() {
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector("h3")!.textContent = this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
